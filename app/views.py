03e75cf91ad801b9c495abd0c7a76c289bb671a0
Cavel Peck <cavelpeck@gmail.com>
2022-03-05 14:29:28 -0500

    your commit message

--- app/views.py
+++ app/views.py
@@ -36,21 +36,15 @@ def login():
           return redirect(url_for('secure_page'))
         # change this to actually validate the entire form submission
         # and not just one field
-    form = LoginForm()
+    if request.method == "POST" and form.validate_on_submit():
 
-    if form.validate_on_submit():
+     if form.username.data:
             # Get the username and password values from the form.
         username = form.username.data
         password = form.password.data
+
         user = UserProfile.query.filter_by(username=username).first()
         if user is not None and check_password_hash(user.password, password):
-            remember_me = False
-
-            if 'remember_me' in request.form:
-                remember_me = True
-
-               
-
             # using your model, query database for a user based on the username
             # and password submitted. Remember you need to compare the password hash.
             # You will need to import the appropriate function to do so.
@@ -58,10 +52,9 @@ def login():
             # passed to the login_user() method below.
 
             # get user id, load into session
-            login_user(user, remember_me)
+            login_user(user)
             flash('Logged in sucessfully.' 'success')
-            next_page = request.args.get('next')
-            return redirect(next_page or url_for('home'))
+            return redirect(url_for('secure_page'))
         else:
             flash('Username or Password is incorrect.', 'danger')
             # remember to flash a message to the user
@@ -69,7 +62,7 @@ def login():
     flash_errors(form)
     return render_template("login.html", form=form)
 
-@app.route('/secure_page')
+@app.route('/secure_page/')
 @login_required
 def secure_page():
     return render_template('secure_page.html')
